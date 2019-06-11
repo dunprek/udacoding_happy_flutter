1f35ca32675683db5d58341231a3dba34e1acb42
Gideon <Gideon.tobing@Hotmail.com>
2019-06-11 17:27:57 +0700

    image assets

--- lib/main.dart
+++ lib/main.dart
@@ -22,125 +22,27 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: PageAlertDialog(),
+      home: PageImageAsset(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
-
-class PageAlertDialog extends StatefulWidget {
-  @override
-  _PageAlertDialogState createState() => _PageAlertDialogState();
-}
-
-class _PageAlertDialogState extends State<PageAlertDialog> {
-  final GlobalKey<ScaffoldState> _key = new GlobalKey<ScaffoldState>();
-  SimpleDialog simpleDialog;
-
-  void showSnackbar() {
-    _key.currentState
-        .showSnackBar(new SnackBar(content: new Text('This is Snackbar')));
-  }
-
-  void showSimpleDialog() {
-    simpleDialog = new SimpleDialog(
-      title: new Text('Warning'),
-      children: <Widget>[
-        new SimpleDialogOption(
-          child: Text('Jakarta'),
-          onPressed: () {
-            print("jkt");
-          },
-        ),
-        new SimpleDialogOption(
-          child: Text('Padang'),
-          onPressed: () {
-            print("pdg");
-          },
-        ),
-        new SimpleDialogOption(
-          child: Text('Close'),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        )
-      ],
-    );
-    showDialog(context: context, child: simpleDialog);
-  }
-
-  void showAlertDialog() {
-    showDialog(
-        context: context,
-        child: new AlertDialog(
-          title: Text('Warning'),
-          content: Text('Anda yakin ingin keluar?'),
-          actions: <Widget>[
-            new IconButton(
-                icon: Icon(Icons.close),
-                onPressed: () {
-                  Navigator.pop(context);
-                }),
-            new IconButton(
-                icon: Icon(Icons.check),
-                color: Colors.green,
-                onPressed: () {
-                  Navigator.pop(context);
-                }),
-          ],
-        ));
-  }
-
+class PageImageAsset extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: new Text('Notification Widget'),
+        title: Text('Image pada asset'),
         backgroundColor: Colors.green,
       ),
-      key: _key,
-      body: new Center(
-        child: new Column(
-          children: <Widget>[
-            new RaisedButton(
-              onPressed: () {
-                showSimpleDialog();
-              },
-              child: new Text('Show Alert Dialog'),
-            ),
-            new MaterialButton(
-              onPressed: () {
-                showAlertDialog();
-              },
-              child: Text('Alert Dialog'),
-              color: Colors.blue,
-              textColor: Colors.white,
-            ),
-            new MaterialButton(
-              onPressed: () {
-                showSnackbar();
-              },
-              child: Text('Show Snackbar'),
-              color: Colors.orange,
-              textColor: Colors.white,
-            ),
-            new MaterialButton(
-              onPressed: () {
-                Fluttertoast.showToast(
-                  msg: "Ini Toast",
-                  toastLength: Toast.LENGTH_SHORT,
-                  gravity: ToastGravity.BOTTOM,
-                  backgroundColor: Colors.green,
-                  textColor: Colors.white
-                );
-              },
-              child: Text('Show Toast'),
-              color: Colors.purple,
-              textColor: Colors.white,
-            )
-          ],
-        ),
+
+      body: Column(
+        children: <Widget>[
+          Image.asset('gambar/13301518.jpeg'),
+          Text('Tayo')
+        ],
       ),
     );
   }
 }
+
